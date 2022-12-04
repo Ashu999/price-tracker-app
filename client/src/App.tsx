eb9abbe6426e02db62a4e60e849c8b6cd6c7accc
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2022-12-04 16:43:11 +0530

    clinet: added SessionAuth on root UI component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { AppLayout } from './layout/AppLayout';
 import './app.css';
+import { SessionAuth } from 'supertokens-auth-react/recipe/session';
 
 function App() {
   return (
     <div>
-      <AppLayout />
+      <SessionAuth>
+        <AppLayout />
+      </SessionAuth>
     </div>
   );
 }
