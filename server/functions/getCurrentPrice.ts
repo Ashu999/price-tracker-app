dd145856dece8cc8f70e5a23e9dd94ce9a26dbbc
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2022-12-11 13:50:13 +0530

    server: faster getCurrentPrice (40sec to 5sec)

--- server/functions/getCurrentPrice.ts
+++ server/functions/getCurrentPrice.ts
@@ -1,20 +1,37 @@
 const puppeteer = require('puppeteer');
 
-async function getCurrentPrice(itemUrl: String) {
+export async function getCurrentPrice(itemUrl: String) {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox'],
   });
   const page = await browser.newPage();
-  await page.goto(itemUrl);
+  //Disable Loading Images (reduced runtime by more than half)
+  await page.setRequestInterception(true);
+  page.on('request', (req: any) => {
+    if (
+      req.resourceType() === 'image' ||
+      req.resourceType() == 'stylesheet' ||
+      req.resourceType() == 'font'
+    ) {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+  // Configure the navigation timeout
+  await page.setDefaultNavigationTimeout(0);
+  //domcontentloaded does not load javascript (reduced runtime by half)
+  await page.goto(itemUrl, { waitUntil: 'domcontentloaded' });
   const curPrice = await page.evaluate(() => {
     return document.querySelector('.a-offscreen')?.innerHTML;
   });
 
   console.log('Cur Price: ', curPrice);
-  await browser.close();
+  browser.close();
+  return curPrice;
 }
 
-const itemUrl =
-  'https://www.amazon.in/dp/B0728CZSKC?ref_=cm_sw_r_cp_ud_dp_BN1PWEVV1PAC47K38XWF';
-getCurrentPrice(itemUrl);
+// const itemUrl =
+//   'https://www.amazon.in/dp/B0728CZSKC?ref_=cm_sw_r_cp_ud_dp_BN1PWEVV1PAC47K38XWF';
+// getCurrentPrice(itemUrl);
