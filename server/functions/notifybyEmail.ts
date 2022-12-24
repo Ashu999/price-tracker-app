ee09dbd1ad954a7b5491c4cb15b3bc9a86e8f6c7
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2022-12-24 21:56:16 +0530

    server: added nodemailer for sending emails

--- server/functions/notifybyEmail.ts
+++ server/functions/notifybyEmail.ts
@@ -1,1 +1,25 @@
-//takes custome message and send Email to user
+//takes custome message, userEmailID and send Email to user
+import nodemailer from 'nodemailer';
+
+export async function notifyByEmail(userEmail: string, message: string) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.NOTIFICATION_SENDER_EMAIL,
+      pass: process.env.NOTIFICATION_SENDER_PASS,
+    },
+  });
+  let mailDetails = {
+    from: `"Price Tracker App" <${process.env.NOTIFICATION_SENDER_EMAIL}>`,
+    to: userEmail,
+    subject: 'Price Tracker App Notification',
+    text: message,
+  };
+  transporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.error('Error Occurs: ', err);
+    } else {
+      console.log('Email sent successfully');
+    }
+  });
+}
