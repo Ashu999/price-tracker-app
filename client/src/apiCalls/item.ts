ad3b28cf6d60155064a856e3562c4c82a6c712d4
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2022-12-30 18:45:08 +0530

    client: code for calling APIs

--- client/src/apiCalls/item.ts
+++ client/src/apiCalls/item.ts
@@ -1,12 +1,38 @@
-async function postFormData(params: Object) {
-  const rawResponse = await fetch('https://httpbin.org/post', {
+const serverAddress: string =
+  process.env.REACT_APP_SERVER_ADDRESS || 'http://localhost:80';
+
+export async function addItemAPI(item: any) {
+  const rawResponse = await fetch(`${serverAddress}/item`, {
     method: 'POST',
-    // headers: {
-    //   Accept: 'application/json',
-    //   'Content-Type': 'application/json',
-    // },
-    body: JSON.stringify({ a: 1, b: 'Textual content' }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(item),
   });
   const content = await rawResponse.json();
   console.log(content);
+}
+
+export async function getUserItemsAPI() {
+  const rawResponse = await fetch(`${serverAddress}/item`, {
+    method: 'GET',
+  });
+  const content = await rawResponse.json();
+  console.log(content);
+  return content;
+}
+
+export async function deleteItemAPI(key: string) {
+  const rawResponse = await fetch(`${serverAddress}/item`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id: key }),
+  });
+  const content = await rawResponse.json();
+  console.log(content);
+  return content;
 }
