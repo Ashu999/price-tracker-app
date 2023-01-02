d1304299506e27b54732ddb2e532a84285b19ab7
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2023-01-02 18:17:51 +0530

    server: get usedid and mail from db

--- server/dbOperations/User.ts
+++ server/dbOperations/User.ts
@@ -4,3 +4,12 @@ import { deleteUser } from 'supertokens-node';
 export async function deleteUserById(userId: string) {
   await deleteUser(userId); // this will succeed even if the userId didn't exist.
 }
+
+export async function getUserIdAndEmail() {
+  return await prisma.passwordless_users.findMany({
+    select: {
+      user_id: true,
+      email: true,
+    },
+  });
+}
