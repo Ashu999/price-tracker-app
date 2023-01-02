1383b487f6fc15f4e32c89fabad836ce01077286
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2023-01-02 18:22:58 +0530

    server: cron job for sending mail

--- server/cronJob/priceCheckAndNotify.ts
+++ server/cronJob/priceCheckAndNotify.ts
@@ -1,10 +1,24 @@
 import { CronJob } from 'cron';
+import { getNotifactionInfo } from '../functions/getNotifactionInfo';
+import { notifyByEmail } from '../functions/notifyByEmail';
 
-function CheckForPriceChanges() {
-  console.log('SUP');
+async function sendNotification() {
+  console.log('CronJob Triggered');
+  const notifcationInfo = await getNotifactionInfo();
+  console.log('NOTI :', notifcationInfo);
+  for (const info of notifcationInfo) {
+    if (info.email) {
+      notifyByEmail(info.email, info.message);
+    } else {
+      console.error('Email Not Found for: ', info.userId);
+    }
+  }
 }
 
-// create a new CronJob to run the function at 8AM and 5PM every day
-export const job = new CronJob('0 8,17 * * *', CheckForPriceChanges);
+// create a new CronJob to run the function at 8AM, 2PM and 8PM every day
+export const job = new CronJob('0 8,14,20 * * *', sendNotification);
+
+// 06:07PM
+// export const job = new CronJob('7 18 * * *', sendNotification);
 
 console.log('Cron Job Started');
