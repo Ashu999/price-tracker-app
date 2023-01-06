d5cb43b1b63d6b3d3c7f14bddcb52aa9cc2a4765
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2023-01-06 13:48:26 +0530

    server: added RETRY for ERR_NAME_NOT_RESOLVED

--- server/functions/getCurrentPrice.ts
+++ server/functions/getCurrentPrice.ts
@@ -22,13 +22,32 @@ export async function getCurrentPrice(itemUrl: string) {
   });
   // Configure the navigation timeout
   page.setDefaultNavigationTimeout(0);
-  //domcontentloaded does not load javascript (reduces function runtime)
-  await page
-    .goto(itemUrl, { waitUntil: 'domcontentloaded' })
-    .catch((error) => console.error('Error at page.goto: ', error));
-  const curPrice = await page.evaluate(() => {
-    return document.querySelector('.a-offscreen')?.innerHTML;
-  });
+
+  let curPrice: string = 'RETRY';
+  let attempts = 0;
+  while (curPrice === 'RETRY' && attempts < 5) {
+    curPrice = await collectData(itemUrl, page);
+    attempts += 1;
+    if (curPrice === 'RETRY') {
+      // Wait a few seconds, also a good idea to swap proxy here*
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+  }
   browser.close();
   return curPrice;
 }
+
+const collectData = async (itemUrl: string, page: puppeteer.Page) => {
+  try {
+    //domcontentloaded does not load javascript (reduces function runtime)
+    await page.goto(itemUrl, { waitUntil: 'domcontentloaded' });
+    const curPrice = await page.evaluate(() => {
+      return document.querySelector('.a-offscreen')?.innerHTML;
+    });
+    if (typeof curPrice === 'undefined') return 'RETRY';
+    return curPrice;
+  } catch (err) {
+    console.error(err);
+    return 'RETRY';
+  }
+};
